dcf1def2dbba589991049f6584583ba3b476e57c
GiCharkviani <gi.charkviani@gmail.com>
2021-11-18 18:09:51 +0400

    today was a good

--- JS/01-Fundamentals-Part-1/starter/script.js
+++ JS/01-Fundamentals-Part-1/starter/script.js
@@ -1,14 +1,94 @@
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2019;
+// const firstName = 'Jonas';
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2021;
 
-console.log(now - 1991 > now - 2018);
-console.log(25-10-5);
+// const jonas = "i'm" + firstName + ", a " + (year - birthYear) + " years old " + job + '!';
 
-let x, y;
-x = y = 25-10-5;
-console.log(x, y);
+// console.log(jonas);
 
-const avarageAge = (ageJonas + ageSarah)/2;
+// const jonasNew = `I am ${firstName}, a ${year-birthYear} years old ${job}!`
+// console.log(jonasNew);
 
-console.log(ageJonas, ageSarah, avarageAge);
+// console.log(`Just a regular string...`);
+
+// console.log('String with \n\
+// multiple \n\
+// lines \n\
+// ');
+
+// console.log(`String
+// multiple
+// lines`);
+
+// const age = 15;
+
+// if(age >= 18) {
+//     console.log('Sarah can start driving license 💋')
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years 😁`)
+// }
+
+// // type conversion
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(typeof NaN);
+
+// console.log(String(45445), 23);
+
+// // type coercion
+// console.log('I am ' + 23 + 10 + ' years old.');
+// console.log('23' - '10' - 3);
+// console.log('23' * '2' - 3);
+// console.log('23' / '2' - 3);
+
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+// console.log(Boolean(0))
+
+// const money = '0' - 0;
+// if(money) {
+//     console.log("Don't spend it all 🤦‍♀️")
+// }else {
+//     console.log("You should get a job 🐱‍🏍")
+// }
+
+// let height = '0';
+// if(height) {
+//     console.log("YAY! Height is defined ✌")
+// }else {
+//     console.log("Height is UNDEFINED 🐱‍👤")
+// }
+
+// const age = '18';
+// if(age === 18) console.log('You just became an adult 🎶 (strict)');
+// if(age == 18) console.log('Smile 😍');
+
+// const favourite = prompt("What's your favourite number?");
+// console.log(typeof +favourite)
+
+// const day = 'wednesday';
+
+// switch(day) {
+//     case 'monday': // day === 'monday'
+//         console.log('we got mondaaayyyy 🤷‍♀️');
+//     break;
+//     case 'tuesday': //similar cases
+//     case 'wednesday':
+//         console.log('this is tuesdaaayyyy or wednesdayyyyyyy 👀');
+//     break;
+//     default:
+//         console.log('ya ya ya, let us dance 🐱‍🐉')
+// }
+
+// const age = 18;
+// age >= 18 ? console.log("I like to drink wine 🍇") : console.log("I like to drink water 🧊");
+
+// const drink = age >= 18 ? "wine 🍇" : "water 🧊";
+// console.log(drink);
+
+// console.log(`I like to drink ${age >= 18 ? "wine 🍇" : "water 🧊"}`)
