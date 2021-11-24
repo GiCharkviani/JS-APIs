14a027a61aa70409d4c6799aa780bb82aa229751
GiCharkviani <gi.charkviani@gmail.com>
2021-11-24 18:31:59 +0400

    finished basics

--- JS/02-Fundamentals-Part-2/starter/script.js
+++ JS/02-Fundamentals-Part-2/starter/script.js
@@ -81,3 +81,20 @@ const calcAge = jonas.calcAge()
 console.log(calcAge)
 console.log(jonas.getSummery())
 
+// for(let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep} 🍜`)
+// }
+
+let rep = 1;
+while(rep <= 10) {
+    console.log(`WHILE: Lifting weights repetition ${rep} 🍜`)
+    rep++
+}
+
+let dice = Math.trunc(Math.random()*6)+1;
+console.log(dice)
+while(dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random()*6)+1;
+    if(dice === 6 ) console.log('loop is about to end...')
+}
