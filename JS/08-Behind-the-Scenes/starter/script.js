22746019f94c4380f4c110bb75af77edd8551216
GiCharkviani <gi.charkviani@gmail.com>
2021-12-16 12:49:22 +0400

    jonas jonas

--- JS/08-Behind-the-Scenes/starter/script.js
+++ JS/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,49 @@
-'use strict';
+// 'use strict';
+
+// function calcAge(bithYear) {
+//   const age = 2037 - bithYear;
+
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${bithYear}`;
+//     console.log(output);
+
+//     if (bithYear >= 1981 && bithYear <= 1996) {
+//       var millenial = true;
+//       // Creating NEW variable with same name as outer scope's variable
+//       const firstName = 'Steven';
+
+//       // Reassigning outer scope's variable
+//       output = 'NEW OUTPUT';
+
+
+//       const str = `oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
+
+//       function add(a, b) {
+//           return a+b
+//       }
+//     }
+//     // console.log(str)
+//     console.log(millenial);
+//     // add(2,3)
+//     console.log(output)
+//   }
+
+//   printAge();
+//   return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1994);
+// // console.log(age)
+// // printAge()
+
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'Teacher';
+const year = 1991
