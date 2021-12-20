ee22eebfef6af837f776086e0b147c994ef35e0f
GiCharkviani <gi.charkviani@gmail.com>
2021-12-20 14:38:48 +0400

    today

--- JS/08-Behind-the-Scenes/starter/script.js
+++ JS/08-Behind-the-Scenes/starter/script.js
@@ -15,7 +15,6 @@
 //       // Reassigning outer scope's variable
 //       output = 'NEW OUTPUT';
 
-
 //       const str = `oh, and you're a millenial, ${firstName}`;
 //       console.log(str);
 
@@ -38,42 +37,142 @@
 // // console.log(age)
 // // printAge()
 
+// // Variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
 
-// Variables
-console.log(me);
-// console.log(job);
-// console.log(year);
+// var me = 'Jonas';
+// let job = 'Teacher';
+// const year = 1991
 
-var me = 'Jonas';
-let job = 'Teacher';
-const year = 1991
+// // Functions
+// console.log(addDecl(5,6))
+// // console.log(addExp(5,6))
+// // console.log(addArrow(5,6))
+
+// function addDecl(a,b) {
+//     return a + b;
+// }
+
+// const addExp = function(a,b) {
+//     return a + b;
+// }
+
+// var addArrow = (a,b) => a+b;
+
+// // Example
+
+// if(!numProd) deleteShoppingCart()
+
+// var numProd = 10;
+
+// function deleteShoppingCart() {
+//     console.log('All products deleted!')
+// }
+
+// var x = 1;
+// let y = 3;
+// const z = 3;
+
+// // this keyword
+// console.log(this);
+// const calcAge = function (birthyear) {
+//   console.log(2037 - birthyear);
+// //   console.log(this);
+// };
+// calcAge(1991);
+
+// const calcAgeArrow = birthyear => {
+//   console.log(2037 - birthyear);
+// //   console.log(this);
+// };
+// calcAgeArrow(1991);
+
+// const jonas = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year)
+//   },
+// };
+// jonas.calcAge();
+
+// const matilda = {
+//     year: 2017,
+// }
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// // f();
+
+// Regular VS Arrow Funtions
+
+// var firstName = 'Matlida'; // because it's var and registrates in global window
+
+// const jonas = {
+//   firstName: 'Jonas',
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+
+//     const self = this; // option to store this - called self or that, or instead arrow function to use
+
+//     const isMillenial = function() {
+//         console.log(self)
+//         console.log(self.year >= 1981 && self.year <= 1996)
+//         // console.log(this.year >= 1981 && this.year <= 1996) // won't work as it doesn't have access to parent by this keyword
+//     };
+//     isMillenial()
+//   },
+
+//   greet: () => {
+//     console.log(this); // global
+//     console.log(`Hey ${this.firstName}`); // this - doesn't work in arrow func, because its parent scope is global scope, as there is not "firstName"
+//   },
+// };
+// jonas.greet();
+// console.log(this);
+// jonas.calcAge();
+
+// const addExp = function(a,b) {
+//     console.log(arguments)
+//     return a + b;
+// }
+// addExp(2, 5);
+// addExp(2, 5, 5, 1, 2, 3);
+
+// var addArrow = (a,b) => {
+//     // console.log(arguments) // won't work as this keyword
+//     return a+b
+// };
+// addArrow(5,1);
 
 
-// Functions
-console.log(addDecl(5,6))
-// console.log(addExp(5,6))
-// console.log(addArrow(5,6))
+// Primitives VS Objects
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(age);
+// console.log(oldAge);
 
-function addDecl(a,b) {
-    return a + b;
-}
+// const me = {
+//     name: 'ComputerX',
+//     age: 30
+// }
+// const friend = me;
+// friend.age = 27;
+// console.log('Friend: ', friend);
+// console.log('Me: ', me);
 
-const addExp = function(a,b) {
-    return a + b;
-}
+// let lastName = 'Williams';
+// let oldLastName = lastName;
+// lastName = 'Davids';
+// console.log(lastName, oldLastName)
 
-var addArrow = (a,b) => a+b;
-
-// Example
-
-if(!numProd) deleteShoppingCart()
-
-var numProd = 10;
-
-function deleteShoppingCart() {
-    console.log('All products deleted!')
-}
-
-var x = 1;
-let y = 3;
-const z = 3;
+var david = 'David'
+let david1 = 'David'
+const david2 = 'David'
